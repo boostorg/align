4bbfff78c5ca8021212dd240ce890fc7951d92a9
Glen Fernandes <glen.fernandes@gmail.com>
2017-05-03 08:59:38 -0400

    Simplify aligned_delete unit test

--- test/aligned_delete_test.cpp
+++ test/aligned_delete_test.cpp
@@ -11,13 +11,11 @@ Distributed under the Boost Software License, Version 1.0.
 #include <boost/core/lightweight_test.hpp>
 #include <new>
 
-template<class T>
 class type {
 public:
     static unsigned count;
 
-    type()
-        : value_() {
+    type() {
         ++count;
     }
 
@@ -26,44 +24,28 @@ public:
     }
 
 private:
-    T value_;
+    type(const type&);
+    type& operator=(const type&);
 };
 
-template<class T>
-unsigned type<T>::count = 0;
-
-template<class T>
-void test()
-{
-    typedef type<T> E;
-    void* p = boost::alignment::aligned_alloc(boost::
-        alignment::alignment_of<E>::value, sizeof(E));
-    BOOST_TEST(p != 0);
-    E* q = ::new(p) E;
-    BOOST_TEST(E::count == 1);
-    boost::alignment::aligned_delete()(q);
-    BOOST_TEST(E::count == 0);
-}
-
-class C { };
-union U { };
+unsigned type::count = 0;
 
 int main()
 {
-    test<char>();
-    test<bool>();
-    test<short>();
-    test<int>();
-    test<long>();
-    test<float>();
-    test<double>();
-    test<long double>();
-    test<void*>();
-    test<void(*)()>();
-    test<C>();
-    test<int C::*>();
-    test<int (C::*)()>();
-    test<U>();
-
+    {
+        void* p = boost::alignment::aligned_alloc(1, 1);
+        char* q = ::new(p) char;
+        boost::alignment::aligned_delete()(q);
+    }
+    {
+        enum {
+            N = boost::alignment::alignment_of<type>::value
+        };
+        void* p = boost::alignment::aligned_alloc(N, sizeof(type));
+        type* q = ::new(p) type;
+        BOOST_TEST(type::count == 1);
+        boost::alignment::aligned_delete()(q);
+        BOOST_TEST(type::count == 0);
+    }
     return boost::report_errors();
 }
